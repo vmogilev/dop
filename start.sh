c9cbedc5f37831d38d1a73116e8413ed8fa31a6e
vmogilev <oracle.ux@gmail.com>
2015-05-25 06:49:01 -0700

    changed to use GOBIN in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-/data/app/dev/golang/bin/dop -dopRoot="/data/app/rigingo.com/dop" \
+$GOBIN/dop -dopRoot="/data/app/rigingo.com/dop" \
     -httpHost="http://rigingo.com/" \
     -httpMount="/dop" \
     -httpPort="80" >> /data/app/rigingo.com/dop/server.log 2>&1
