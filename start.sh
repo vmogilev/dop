2df55566b130cdb9174bb5d2336e29baf85c84db
vmogilev <oracle.ux@gmail.com>
2015-05-30 22:35:53 -0700

    new httpHostExt to handle nginx proxy and such

--- start.sh
+++ start.sh
@@ -18,9 +18,11 @@ else
 fi
 
 
-nohup $GOPATH/bin/dop -dopRoot="${DOPROOT}" \
+nohup $GOPATH/bin/dop \
+    -dopRoot="${DOPROOT}" \
     -httpHost="${HTTPHOST}" \
     -httpMount="${HTTPMOUNT}" \
-    -httpPort="${HTTPPORT}" >> ${DOPROOT}/server.log 2>&1 </dev/null &
+    -httpPort="${HTTPPORT}" \
+    -httpHostExt="${HTTPHOSTEXT}" >> ${DOPROOT}/server.log 2>&1 </dev/null &
 
 
